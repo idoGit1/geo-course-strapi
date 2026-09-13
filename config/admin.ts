37f35ccaca9c81f4ee1ac80fdfe3a321cc9c0bd6
Ido Finkelshtein <finkel.ido11@gmail.com>
2026-09-13 19:38:57 +0300

    make url in build

--- config/admin.ts
+++ config/admin.ts
@@ -1,25 +1,27 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Admin => ({
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Admin => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET')!,
+    secret: env("ADMIN_JWT_SECRET")!,
   },
-  url: env('ADMIN_URL', '/admin'),
+  url: "/control-panel",
   apiToken: {
-    salt: env('API_TOKEN_SALT')!,
+    salt: env("API_TOKEN_SALT")!,
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT')!,
+      salt: env("TRANSFER_TOKEN_SALT")!,
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY')!,
+    encryptionKey: env("ENCRYPTION_KEY")!,
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-    docLinks: env.bool('FLAG_DOC_LINKS', true),
+    nps: env.bool("FLAG_NPS", true),
+    promoteEE: env.bool("FLAG_PROMOTE_EE", true),
+    docLinks: env.bool("FLAG_DOC_LINKS", true),
   },
 });
 
